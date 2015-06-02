538ee27c15fcb0a4164d98c96017eeeff257b3f0
Daisy Molving <daisymolving@gmail.com>
2015-06-02 20:34:56 +1200

    final commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  
+  # CONVENTION OVER CONFIGURATION, look in views for a folder with the same name as the controller, and a file with the same name as the action
+  def welcome  
+  end
+
+  def intro  
+  end
+
+  def portfolio 
+  end
+
+  def bio
+  end
+
+  def contact_info
+  end
+
+  
+end
